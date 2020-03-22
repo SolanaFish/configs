d9e7b9668be7cc04c0d69d40c1493d987633c783
Michał Bartoszewicz <michal.bartoszewicz@grupawp.pl>
2020-03-22 08:55:05 +0100

    Backup march 2020

---
+++ mac/fish/config.fish
@@ -0,0 +1,6 @@
+set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/lib/ruby/gems/2.5.0/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
+set -g fish_user_paths "/Users/mbartoszewicz/pear/share/pear" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/node@12/bin" $fish_user_paths
+neofetch
